51160ddbb8a1d20b46dbe9591cef3d65d6ce4ffb
John Eastman <radon@Johns-MacBook-Pro.local>
2022-08-25 16:13:47 -0600

    edit contacts

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,5 +1,26 @@
 class ComplaintsController < ApplicationController
     def index
-        @contact_info = ContactInfo.all
+        @contacts = ContactInfo.all
+    end
+
+    def show
+        @contact = ContactInfo.find(params[:id])
+    end
+
+    def edit
+        @contact_info = ContactInfo.find(params[:id])
+    end
+
+    def update
+        @contact = ContactInfo.find(params[:id])
+
+        @contact.update(contact_params)
+        redirect_to complaint_path(@contact)
+    end
+
+    private
+
+    def contact_params
+        params.require(:contact_info).permit(:first_name, :last_name, :mailing_address)
     end
 end
