1c3364c2f22c3d6924dcd106d3260ba5acb56f08
John Eastman <radon@Johns-MacBook-Pro.local>
2022-08-26 18:24:07 -0600

    edit employees

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -7,4 +7,40 @@ class EmployersController < ApplicationController
 		@employer = Employer.find(params[:id])
 		@complaints = @employer.complaints
 	end
+
+	def new
+		@employer = Employer.new
+	end
+
+	def create
+		employer = Employer.new(employer_params)
+		employer.save
+		redirect_to employers_path
+	end
+
+	def edit
+		@employer = Employer.find(params[:id])
+	end
+
+	def update
+		employer = Employer.find(params[:id])
+		employer.update(employer_params)
+		redirect_to employer_path(employer)
+	end
+
+	private
+
+	def employer_params
+		params.require(:employer).permit(
+			:name,
+			:contact_person_name,
+			:mailing_address,
+			:city,
+			:state,
+			:zipcode,
+			:phone_number,
+			:email_address,
+			:website
+		)
+	end
 end
