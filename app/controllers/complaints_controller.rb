71cfb5b48ba74268066edda6f042c107551433cf
John Eastman <radon@Johns-MacBook-Pro.local>
2022-09-13 18:02:36 -0600

    download files

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -32,6 +32,27 @@ class ComplaintsController < ApplicationController
 		redirect_to employer_complaints_path(@employer), status: :see_other
 	end
 
+	def generate_complaint
+		user = current_user
+		employer = Employer.find(params[:employer_id])
+		complaint = Complaint.find(params[:id])
+
+		# TEMPORARY
+		filename = "random.txt"
+		File.open(filename, "w") do |f|
+			f.write("Your random number is #{rand(10)}")
+		end
+
+		# TODO: Populate PDF form and download
+
+		send_file(
+			"#{Rails.root}/#{filename}",
+			filename: filename,
+			type: "text/plain",
+		)
+	end
+	helper_method :generate_complaint
+
 	private 
 
 	def complaint_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :users
   resources :employers do
     resources :complaints
+
+    get "complaints/:id/generate" => "complaints#generate_complaint", as: :generate_complaint
   end
 
   resource :session, only: [:new, :create, :destroy]
