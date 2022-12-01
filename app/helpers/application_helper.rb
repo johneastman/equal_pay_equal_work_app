271b427413923c17fdb1e5072a556e50df64441e
John Eastman <radon@Johns-MBP.hsd1.co.comcast.net>
2022-12-01 14:23:10 -0700

    css formatting for header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def welcome_display
+    message = "Welcome"
+    if !current_user.nil? && current_user.first_name.present?
+      message += ", #{current_user.first_name}!"
+    else
+      message += "!"
+    end
+    return message
+  end
 end
